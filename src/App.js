a099dfbfe2a2f28bf880844479c489c507a6b8dd
Anas DADI <dadi.anas@hotmail.fr>
2021-06-09 03:41:26 +0100

    dockerized solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import header_component from './components/header.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <header_component />
+        </header>
+        <div className="grafana">
+          <iframe src="http://10.242.148.48:3000/d-solo/K9vm_w6Mz/zabbix-servers?orgId=1&from=1623183640494&to=1623205240494&panelId=12" width="100%" height="300" frameborder="0" />
+        </div>
+
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header_component() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default header_component
